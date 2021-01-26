f1b73ef4ba5ec4e964d8bd9a03e60daaf90e7813
suyong <yong.su@phmedtech.com>
2021-01-26 15:38:59 +0800

    增加变量

--- test_config/param_config.py
+++ test_config/param_config.py
@@ -1,5 +1,4 @@
-import psycopg2, os
-
+import datetime
 api_url = 'http://192.168.10.254:9191/server/api/1.0'
 # 管理员帐号
 loginName = 'operatorAdmin'
@@ -16,3 +15,11 @@ supplierName02 = '嘉事（上海）国润医疗器械有限公司'
 # 有库存的物资id
 goodsId = 249
 kitTemplateId = 21
+
+######
+# 以下参数不需要修改
+######
+# 初始物资的名称(不需要修改)
+departmentName = '默认部门'
+# 新增基础数据的编号，年月日小时，每小时内重复执行不会再次新增
+count = int('{0:%Y%m%d%H}'.format(datetime.datetime.now()))
