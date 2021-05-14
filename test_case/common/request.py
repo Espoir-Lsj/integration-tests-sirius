013e539e6ed54d720f764163e9542c9debcf559a
liangshoujie <L1768154@163.com>
2021-05-14 13:30:39 +0800

    增加递归替换body key value方法

--- test_case/common/request.py
+++ test_case/common/request.py
@@ -11,6 +11,22 @@ api_url = param_config.api_url
 log = logger.Log()
 
 
+def reValue(body, data):
+    for i in body.keys():
+        if type(body[i]) is str:
+            pass
+        elif type(body[i]) is dict:
+            reValue(body[i], data)
+        elif type(body[i]) is list:
+            for j in body[i]:
+                if type(j) is dict:
+                    reValue(j, data)
+
+        if i in data.keys():
+            body[i] = data[i]
+    return body
+
+
 def get_params(path, params):
     r = requests.get(api_url + path, headers=headers, params=params, verify=False)
     response = r.json()
