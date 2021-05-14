da1c8bed082ef4934697814418dfb59877d7f58b
liangshoujie <L1768154@163.com>
2021-05-14 16:17:42 +0800

    将fixture 统一整理存放

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -4,122 +4,168 @@
 # _*_ coding: utf-8 _*_
 
 import pytest, jsonpath
-from common import request, logger
+from common import request, logger, Purchase_Management
 from test_base_role import createRole, createRoleType
 from test_base_user import createUser
 from test_config import param_config
 from test_base_department import createDept
+from test_config.yamlconfig import timeid
 
 log = logger.Log()
 
 
-# 获取权限列表,并返回所有权限id(数组)
-# 整个项目测试用例执行之前执行一次，无论调用多少次，也只执行一次
-@pytest.fixture(scope="session")
-def getPermissoinIds():
-    """正常查询"""
-    response = request.get('/permission/list')
-    assert response['msg'] == '请求成功'
-    assert len(response['data']) > 0
-    # 获取权限id
-    ids = jsonpath.jsonpath(response, '$..id')
-    return ids
+#
+# # 获取权限列表,并返回所有权限id(数组)
+# # 整个项目测试用例执行之前执行一次，无论调用多少次，也只执行一次
+# @pytest.fixture(scope="session")
+# def getPermissoinIds():
+#     """正常查询"""
+#     response = request.get('/permission/list')
+#     assert response['msg'] == '请求成功'
+#     assert len(response['data']) > 0
+#     # 获取权限id
+#     ids = jsonpath.jsonpath(response, '$..id')
+#     return ids
+#
+#
+# # 创建一个初始角色分类,并返回角色分类id
+# # 整个项目测试用例执行之前执行一次，无聊调用多少次，也只执行一次
+# @pytest.fixture(scope="session")
+# def createInitRoleType():
+#     # 创建角色分类
+#     name = param_config.initRoleTypeName
+#     response = createRoleType(name=name)
+#     assert response['msg'] in ('请求成功', '该角色分类名称已存在')
+#     # 根据角色分类名查询分类id
+#     list = request.get('/role/findRoleTypeList')
+#     assert len(list['data']) > 1
+#     for i in list['data']['roleTypeList']:
+#         if i['name'] == name:
+#             typeId = i['roleTypeId']
+#             return typeId
+#
+#
+# # 创建一个初始角色,并返回角色id
+# # 整个项目测试用例执行之前执行一次，无论调用多少次，也只执行一次
+# @pytest.fixture(scope="session")
+# def createInitRole(createInitRoleType):
+#     # 创建角色
+#     name = param_config.initRoleName
+#     response = createRole(name=name, roleType=createInitRoleType)
+#     assert response['msg'] in ('请求成功', '该角色名称已存在')
+#     # 根据角色名查询角色id
+#     list = request.get('/role/findRoleTypeList')
+#     assert len(list['data']) > 1
+#     # 查询返回结果中所有的角色名
+#     names = jsonpath.jsonpath(list, '$..role[*].name')
+#     # 所有角色id
+#     ids = jsonpath.jsonpath(list, '$..role[*].id')
+#     # 根据角色名查询id
+#     i = 0
+#     while i < len(names):
+#         if names[i] == name:
+#             roleId = ids[i]
+#             return roleId
+#         i += 1
+#
+#
+# # 创建一个用户名为defaultLoginName的用户,并返回用户id
+# # 整个项目测试用例执行之前执行一次，无论调用多少次，也只执行一次(需要调一次执行加上autouse=True)
+# @pytest.fixture(scope="session")
+# def createInitUser(createInitDepartment):
+#     # 账号名称
+#     loginName = param_config.initLoginName
+#     # 密码
+#     initialPassword = param_config.initialPassword
+#     response = createUser(loginName=loginName, initialPassword=initialPassword, departmentId=createInitDepartment)
+#     assert response['msg'] in ('请求成功', '登录名已存在')
+#     # 根据departmentId查询初始用户的id
+#     list = request.get('/user/list?pageNum=0&pageSize=50&departmentId=%s' % createInitDepartment)
+#     assert list['data']['totalCount'] > 0
+#     # 查询返回的所有账号名
+#     names = jsonpath.jsonpath(list, '$..loginName')
+#     # 获取账号id
+#     ids = jsonpath.jsonpath(list, '$..id')
+#     # 根据账号名获取到账号id
+#     i = 0
+#     while i < len(names):
+#         if names[i] == loginName:
+#             id = ids[i]
+#             return id
+#         i += 1
+#
+#
+# # 创建一个初始部门，并返回部门id，整个项目测试用例执行之前执行一次
+# @pytest.fixture(scope="session")
+# def createInitDepartment():
+#     # 初始化一个部门名称
+#     deptName = param_config.initDeptName
+#     # 获取部门id
+#     detail = request.get('/department/findDepartment')
+#     departmentId = detail['data'][0]['id']
+#     # 获取字典部门分类id
+#     detail2 = request.get('/dictionary/getByType/department_type')
+#     sortId = detail2['data'][0]['id']
+#     response = createDept(departmentName=deptName, departmentTypeId=sortId, parentId=departmentId, isEnabled=False)
+#     assert response['msg'] in ('请求成功', '部门已存在')
+#     # 查询列表
+#     list = request.get('/department/findDepartment')
+#     assert len(list['data']) > 0
+#     # 提取所有部门名称
+#     departmentNames = jsonpath.jsonpath(list, '$..childrenDepartment[*].departmentName')
+#     # 提取所有的部门id
+#     ids = jsonpath.jsonpath(list, '$..childrenDepartment[*].id')
+#     # 根据部门名称循环查询部门id
+#     i = 0
+#     while i < len(departmentNames):
+#         if departmentNames[i] == deptName:
+#             id = ids[i]
+#             return id
+#         i += 1
+
+@pytest.fixture(scope='class')
+def res_data():
+    timeid(file_yaml='request_data.yaml')._set_yaml_time({'url': 'body'}, 'w')
 
 
-# 创建一个初始角色分类,并返回角色分类id
-# 整个项目测试用例执行之前执行一次，无聊调用多少次，也只执行一次
-@pytest.fixture(scope="session")
-def createInitRoleType():
-    # 创建角色分类
-    name = param_config.initRoleTypeName
-    response = createRoleType(name=name)
-    assert response['msg'] in ('请求成功', '该角色分类名称已存在')
-    # 根据角色分类名查询分类id
-    list = request.get('/role/findRoleTypeList')
-    assert len(list['data']) > 1
-    for i in list['data']['roleTypeList']:
-        if i['name'] == name:
-            typeId = i['roleTypeId']
-            return typeId
+@pytest.fixture(scope="class")
+# 调拨单
+def AllocateOrder_get_data(res_data):
+    # 获取所有需要的参数
+    test = Purchase_Management.AllocateOrder()
+    global reasonCode, sourceWarehouseId, targetWarehouseId, goodsId, goodsLotInfoId, kitStockId, \
+        goodsQuantity, kitStockQuantity
+    reasonCode = test.get_allocate_reason()
+    sourceWarehouseId = test.get_out_warehouse()
+    targetWarehouseId = test.get_in_warehouse()
+    goodsInfo = test.get_goodsInfo(sourceWarehouseId)
+    goodsId = goodsInfo[0]
+    goodsLotInfoId = goodsInfo[1]
+    kitStockId = test.get_kitStockId(sourceWarehouseId)
+    goodsQuantity = 1
+    kitStockQuantity = 1
 
 
-# 创建一个初始角色,并返回角色id
-# 整个项目测试用例执行之前执行一次，无论调用多少次，也只执行一次
-@pytest.fixture(scope="session")
-def createInitRole(createInitRoleType):
-    # 创建角色
-    name = param_config.initRoleName
-    response = createRole(name=name, roleType=createInitRoleType)
-    assert response['msg'] in ('请求成功', '该角色名称已存在')
-    # 根据角色名查询角色id
-    list = request.get('/role/findRoleTypeList')
-    assert len(list['data']) > 1
-    # 查询返回结果中所有的角色名
-    names = jsonpath.jsonpath(list, '$..role[*].name')
-    # 所有角色id
-    ids = jsonpath.jsonpath(list, '$..role[*].id')
-    # 根据角色名查询id
-    i = 0
-    while i < len(names):
-        if names[i] == name:
-            roleId = ids[i]
-            return roleId
-        i += 1
+@pytest.fixture(scope="class")
+# 获取调拨单id
+def AllocateOrder_get_Id(AllocateOrder_get_data):
+    response = Purchase_Management.AllocateOrder().create(reasonCode, sourceWarehouseId, targetWarehouseId, goodsId,
+                                                          goodsLotInfoId, kitStockId,
+                                                          goodsQuantity, kitStockQuantity)
+    Id = response[0]
+    yield Id
 
 
-# 创建一个用户名为defaultLoginName的用户,并返回用户id
-# 整个项目测试用例执行之前执行一次，无论调用多少次，也只执行一次(需要调一次执行加上autouse=True)
-@pytest.fixture(scope="session")
-def createInitUser(createInitDepartment):
-    # 账号名称
-    loginName = param_config.initLoginName
-    # 密码
-    initialPassword = param_config.initialPassword
-    response = createUser(loginName=loginName, initialPassword=initialPassword, departmentId=createInitDepartment)
-    assert response['msg'] in ('请求成功', '登录名已存在')
-    # 根据departmentId查询初始用户的id
-    list = request.get('/user/list?pageNum=0&pageSize=50&departmentId=%s' % createInitDepartment)
-    assert list['data']['totalCount'] > 0
-    # 查询返回的所有账号名
-    names = jsonpath.jsonpath(list, '$..loginName')
-    # 获取账号id
-    ids = jsonpath.jsonpath(list, '$..id')
-    # 根据账号名获取到账号id
-    i = 0
-    while i < len(names):
-        if names[i] == loginName:
-            id = ids[i]
-            return id
-        i += 1
+@pytest.fixture(scope="class")
+def AllocateOrder_approve(AllocateOrder_get_Id):
+    response = Purchase_Management.AllocateOrder().approve(Id=AllocateOrder_get_Id)
+    yield AllocateOrder_get_Id
 
 
-# 创建一个初始部门，并返回部门id，整个项目测试用例执行之前执行一次
-@pytest.fixture(scope="session")
-def createInitDepartment():
-    # 初始化一个部门名称
-    deptName = param_config.initDeptName
-    # 获取部门id
-    detail = request.get('/department/findDepartment')
-    departmentId = detail['data'][0]['id']
-    # 获取字典部门分类id
-    detail2 = request.get('/dictionary/getByType/department_type')
-    sortId = detail2['data'][0]['id']
-    response = createDept(departmentName=deptName, departmentTypeId=sortId, parentId=departmentId, isEnabled=False)
-    assert response['msg'] in ('请求成功', '部门已存在')
-    # 查询列表
-    list = request.get('/department/findDepartment')
-    assert len(list['data']) > 0
-    # 提取所有部门名称
-    departmentNames = jsonpath.jsonpath(list, '$..childrenDepartment[*].departmentName')
-    # 提取所有的部门id
-    ids = jsonpath.jsonpath(list, '$..childrenDepartment[*].id')
-    # 根据部门名称循环查询部门id
-    i = 0
-    while i < len(departmentNames):
-        if departmentNames[i] == deptName:
-            id = ids[i]
-            return id
-        i += 1
+@pytest.fixture(scope="class")
+def AllocateOrder_close(AllocateOrder_approve):
+    response = Purchase_Management.AllocateOrder().close(AllocateOrder_approve)
+    yield AllocateOrder_approve
 
 
 def pytest_collection_modifyitems(items):
